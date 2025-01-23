294cb4a1aa3d76536aaaeeced41883b8235b947d
HyperkidSJY <shivamyadav9735@gmail.com>
2025-01-23 12:34:54 +0530

    Add project files.

---
+++ BoilerPlate/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace BoilerPlate
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
